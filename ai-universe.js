a3dc6c4d5c563e8ac4cbe3abb16b117a1aca166d
Salehchy46 <salehchyctg@gmail.com>
2024-11-18 02:12:59 +0600

    My first js work

---
+++ ai-universe.js
@@ -0,0 +1,8 @@
+const getTheAiSource = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+  const data = await res.json();
+  const tools = data.data;
+  console.log(tools);
+}
+
+getTheAiSource();
